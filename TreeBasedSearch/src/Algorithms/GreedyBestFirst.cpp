1c403ad09bd76ea1de9015c831fe9b338ace8718
Ruan <ruane.swanepoel@gmail.com>
2022-03-22 20:21:37 +1100

    Updating GBFS

--- TreeBasedSearch/src/Algorithms/GreedyBestFirst.cpp
+++ TreeBasedSearch/src/Algorithms/GreedyBestFirst.cpp
@@ -1,6 +1,15 @@
 #include "GreedyBestFirst.h"
 
-std::pair<Coord, std::map<Coord, Coord>>* GreedyBestFirstAlgorithm::TraceSteps(Grid&) {
+std::pair<Coord, std::map<Coord, Coord>>* GreedyBestFirstAlgorithm::TraceSteps(Grid& grid) {
+
+    Coord agentPosition = grid.GetAgentPosition();
+
+    std::vector<Coord> list;
+    std::vector<Coord> explored;
+    std::map<Coord, Coord> stepTrace;
+
+    list.push_back(agentPosition);
+    explored.push_back(agentPosition);
 
     return nullptr;
 
